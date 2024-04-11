c28e6d47496bb4c92fc2e830b7775b9be459ec3c
AmandaWetter <156665269+AmandaWetter@users.noreply.github.com>
2024-04-11 09:31:03 +0200

    Initial commit

---
+++ linkListDataModel.js
@@ -0,0 +1,5 @@
+renderJSON({"links":[
+    {"nr": "A000", "text":"Sondernutzungsplanung kommunal", "url": "https://amandawetter.github.io/A900/"},
+    {"nr": "A001", "text":"landw. Bewirtschaftungseinheiten", "url": "https://ch-sz-geo.github.io/A001/"},
+    {"nr": "A002","text":"landw. Nutzflächen nach Bundesmodell ", "url": "https://ch-sz-geo.github.io/A002/"}
+]});
