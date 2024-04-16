e70a1c6228b220943962ff5dd8848b78fddf2fdd
AmandaWetter <156665269+AmandaWetter@users.noreply.github.com>
2024-04-16 11:01:06 +0200

    Update linkListDataModel.js

--- linkListDataModel.js
+++ linkListDataModel.js
@@ -1,3 +1,3 @@
 renderJSON({"links":[
-    {"nr": "A001", "text":"Beispielseite der Modelldokumentation", "url": "https://ch-sz-geo.github.io/A001/"},
+    {"nr": "A001", "text":"Beispielseite der Modelldokumentation", "url": "https://ch-sz-geo.github.io/A001/"}
 ]});
