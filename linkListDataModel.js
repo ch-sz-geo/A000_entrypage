4e0f7dfc33572cbc64025a5c2c6c268f7bea7206
peruno <kuno.epper@sz.ch>
2024-07-25 16:18:10 +0200

    Update linkListDataModel.js
    Syntax korrigiert

--- linkListDataModel.js
+++ linkListDataModel.js
@@ -1,4 +1,4 @@
 renderJSON({"links":[
     {"nr": "A001", "text":"Beispielseite der Modelldokumentation", "url": "https://ch-sz-geo.github.io/A001/"}
-    {"nr": "A026", "text":"Ökomorphologie der Fliessgewässer", "url": "https://ch-sz-geo.github.io/A026"}
+    ,{"nr": "A026", "text":"Ökomorphologie der Fliessgewässer", "url": "https://ch-sz-geo.github.io/A026"}
 ]});
