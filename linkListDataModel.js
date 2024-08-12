ba9d81f1d167673514da2494290dec659a87bfad
peruno <kuno.epper@sz.ch>
2024-08-12 08:13:33 +0200

    Update linkListDataModel.js
    A241 hinzugefügt

--- linkListDataModel.js
+++ linkListDataModel.js
@@ -2,4 +2,5 @@ renderJSON({"links":[
      {"nr": "A001", "text":"Beispielseite der Modelldokumentation", "url": "https://ch-sz-geo.github.io/A001/"}
     ,{"nr": "A026", "text":"Ökomorphologie der Fliessgewässer", "url": "https://ch-sz-geo.github.io/A026"}
     ,{"nr": "A057", "text":"Waldfeststellungen", "url": "https://ch-sz-geo.github.io/A057"}
+    ,{"nr": "A241", "text":"Verfahren Waldrecht", "url": "https://ch-sz-geo.github.io/A241"}
 ]});
