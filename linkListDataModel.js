97f7882ce0d4390cc7f309e299d0e536243be027
peruno <kuno.epper@sz.ch>
2024-09-03 16:26:22 +0200

    Update linkListDataModel.js
    A244 hinzugefügt

--- linkListDataModel.js
+++ linkListDataModel.js
@@ -1,6 +1,7 @@
 renderJSON({"links":[
      {"nr": "A026", "text":"Ökomorphologie der Fliessgewässer", "url": "https://ch-sz-geo.github.io/A026"}
     ,{"nr": "A057", "text":"Waldfeststellungen", "url": "https://ch-sz-geo.github.io/A057"}
-    ,{"nr": "A144", "text":"geologische Berichte des Untersuchungsbereichs 'Umwelt'", "url": "https://ch-sz-geo.github.io/A144"}
+    ,{"nr": "A144", "text":"geologische Berichte des Bereichs 'Umwelt'", "url": "https://ch-sz-geo.github.io/A144"}
     ,{"nr": "A241", "text":"Verfahren Waldrecht", "url": "https://ch-sz-geo.github.io/A241"}
+    ,{"nr": "A244", "text":"Bohrdaten des Bereichs 'Umwelt'", "url": "https://ch-sz-geo.github.io/A244"}
 ]});
