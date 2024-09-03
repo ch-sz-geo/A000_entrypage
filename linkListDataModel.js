90da9caa924fd6cc9e940d3d6c67d36a15ad38e0
peruno <kuno.epper@sz.ch>
2024-09-03 11:01:27 +0200

    Update linkListDataModel.js
    Beispiel-Thema gelöscht

--- linkListDataModel.js
+++ linkListDataModel.js
@@ -1,6 +1,5 @@
 renderJSON({"links":[
-     {"nr": "A001", "text":"Beispielseite der Modelldokumentation", "url": "https://ch-sz-geo.github.io/A001/"}
-    ,{"nr": "A026", "text":"Ökomorphologie der Fliessgewässer", "url": "https://ch-sz-geo.github.io/A026"}
+     {"nr": "A026", "text":"Ökomorphologie der Fliessgewässer", "url": "https://ch-sz-geo.github.io/A026"}
     ,{"nr": "A057", "text":"Waldfeststellungen", "url": "https://ch-sz-geo.github.io/A057"}
     ,{"nr": "A144", "text":"geologische Berichte des Untersuchungsbereichs 'Umwelt'", "url": "https://ch-sz-geo.github.io/A144"}
     ,{"nr": "A241", "text":"Verfahren Waldrecht", "url": "https://ch-sz-geo.github.io/A241"}
