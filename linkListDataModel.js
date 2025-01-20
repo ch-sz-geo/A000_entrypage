9664a56cf6ac35f3809446e1f892660b0d111756
peruno <kuno.epper@sz.ch>
2025-01-20 16:23:51 +0100

    Update linkListDataModel.js
    A188 eingetragen

--- linkListDataModel.js
+++ linkListDataModel.js
@@ -3,6 +3,7 @@ renderJSON({"links":[
     ,{"nr": "A057", "text":"Waldfeststellungen", "url": "https://ch-sz-geo.github.io/A057"}
     ,{"nr": "A084", "text":"Richtplan, kantonal", "url": "https://ch-sz-geo.github.io/A084"}
     ,{"nr": "A144", "text":"geologische Berichte des Bereichs 'Umwelt'", "url": "https://ch-sz-geo.github.io/A144"}
+    ,{"nr": "A188", "text":"Rodungen und Rodungsersatz", "url": "https://ch-sz-geo.github.io/A188"}
     ,{"nr": "A241", "text":"Verfahren Waldrecht", "url": "https://ch-sz-geo.github.io/A241"}
     ,{"nr": "A244", "text":"Bohrdaten des Bereichs 'Umwelt'", "url": "https://ch-sz-geo.github.io/A244"}
 ]});
