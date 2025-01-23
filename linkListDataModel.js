8c6b87ce2549743eb64ba29d5962cd5f9ce7109a
peruno <kuno.epper@sz.ch>
2025-01-23 08:20:20 +0100

    Update linkListDataModel.js
    A064 hinzugefügt

--- linkListDataModel.js
+++ linkListDataModel.js
@@ -1,6 +1,7 @@
 renderJSON({"links":[
      {"nr": "A026", "text":"Ökomorphologie der Fliessgewässer", "url": "https://ch-sz-geo.github.io/A026"}
     ,{"nr": "A057", "text":"Waldfeststellungen", "url": "https://ch-sz-geo.github.io/A057"}
+    ,{"nr": "A064", "text":"forstliche Planung; Bereich Waldfunktion (Schutzwald)", "url": "https://ch-sz-geo.github.io/A064"}
     ,{"nr": "A084", "text":"Richtplan, kantonal", "url": "https://ch-sz-geo.github.io/A084"}
     ,{"nr": "A144", "text":"geologische Berichte des Bereichs 'Umwelt'", "url": "https://ch-sz-geo.github.io/A144"}
     ,{"nr": "A188", "text":"Rodungen und Rodungsersatz", "url": "https://ch-sz-geo.github.io/A188"}
