007db507069137c4cf3d1be6c0c4364661134fe1
peruno <kuno.epper@sz.ch>
2025-02-04 13:58:47 +0100

    Update linkListDataModel.js

--- linkListDataModel.js
+++ linkListDataModel.js
@@ -4,7 +4,7 @@ renderJSON({"links":[
     ,{"nr": "A064", "text":"forstliche Planung; Bereich Waldfunktion (Schutzwald)", "url": "https://ch-sz-geo.github.io/A064"}
     ,{"nr": "A084", "text":"Richtplan, kantonal", "url": "https://ch-sz-geo.github.io/A084"}
     ,{"nr": "A144", "text":"geologische Berichte des Bereichs 'Umwelt'", "url": "https://ch-sz-geo.github.io/A144"}
-    ,{"nr": "A163", "text":"Planung der Revitalisierungen von Gewässern (Bereich Standgewässer)", "url": "https://ch-sz-geo.github.io/A163"}
+    ,{"nr": "A163", "text":"Planung der Revitalisierung von Seeufern", "url": "https://ch-sz-geo.github.io/A163"}
     ,{"nr": "A188", "text":"Rodungen und Rodungsersatz", "url": "https://ch-sz-geo.github.io/A188"}
     ,{"nr": "A241", "text":"Verfahren Waldrecht", "url": "https://ch-sz-geo.github.io/A241"}
     ,{"nr": "A244", "text":"Bohrdaten des Bereichs 'Umwelt'", "url": "https://ch-sz-geo.github.io/A244"}
